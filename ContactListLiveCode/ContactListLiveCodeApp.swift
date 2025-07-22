9dfc62c0cb03d3de084a0c5d8d512ee4afe92310
TomHuynhSG <tomhuynhsg@gmail.com>
2025-07-22 15:07:37 +0700

    Initial Commit

---
+++ ContactListLiveCode/ContactListLiveCodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContactListLiveCodeApp.swift
+//  ContactListLiveCode
+//
+//  Created by Tom Huynh on 22/7/25.
+//
+
+import SwiftUI
+
+@main
+struct ContactListLiveCodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ContactListLiveCode/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ContactListLiveCode
+//
+//  Created by Tom Huynh on 22/7/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
